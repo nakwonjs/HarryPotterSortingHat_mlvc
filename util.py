166c872c95681a14093d79a79573fe46b4a2be00
Wawtuh <65636687+Wawtuh@users.noreply.github.com>
2021-11-16 21:02:48 +0900

    gitignore

---
+++ util.py
@@ -0,0 +1,14 @@
+import torch
+
+def check_device():
+	print("### Device Check list ###")
+	is_cuda =  torch.cuda.is_available()
+	print("GPU available?:", torch.cuda.is_available())
+	if is_cuda:
+		device_number = torch.cuda.current_device()
+		print("Device number:", device_number)
+		print("Is device?:", torch.cuda.device(device_number))
+		print("Device count?:", torch.cuda.device_count())
+		print("Device name?:", torch.cuda.get_device_name(device_number))
+	print("### ### ### ### ### ###\n\n")
+	return torch.device('cuda' if is_cuda else 'cpu')
