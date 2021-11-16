aea0d8e16e6cd2ab5ae3406b4c800a9b63ae0e4c
Wawtuh <65636687+Wawtuh@users.noreply.github.com>
2021-11-16 20:32:19 +0900

    Initial commit

---
+++ Crawling.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+import time
+from urllib.request import (urlopen, urlparse, urlunparse, urlretrieve)
+
+chrome_path ='Data\Gryffindor'
+base_url = "https://www.google.co.kr/imghp"
